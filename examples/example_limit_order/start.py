45cecbde79ba171ffbeba3397be661d90a9d71d2
Philipp  Schmitz <philipp.schmitz@tu-ilmenau.de>
2023-05-12 19:23:51 +0200

    ...

--- examples/example_limit_order/start.py
+++ examples/example_limit_order/start.py
@@ -4,12 +4,15 @@ import multiprocessing
 
 if __name__ == '__main__':  
     while 1:
-        p = multiprocessing.Process(target=foo, name="foo")
-        p.start()
-        time.sleep(360)
-        p.terminate()
-        p.join()
-        print('Restart in...')
-        for i in range(10,0,-1):
-            print(i)
-            time.sleep(1)
+        try:
+            p = multiprocessing.Process(target=foo, name="foo")
+            p.start()
+            time.sleep(360)
+            p.terminate()
+            p.join()
+            print('Restart in...')
+            for i in range(10,0,-1):
+                print(i)
+                time.sleep(1)
+        except:
+            print("Blabla")
